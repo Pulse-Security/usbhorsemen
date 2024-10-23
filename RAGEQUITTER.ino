6c2894825e25b70729905da38954a9c4c13494da
DoI <5291556+denandz@users.noreply.github.com>
2024-10-23 19:12:28 +1300

    Create RAGEQUITTER.ino

---
+++ RAGEQUITTER.ino
@@ -0,0 +1,78 @@
+#include "Keyboard.h"
+
+uint8_t modifier = 0;
+uint8_t code = 0;
+int loops = 0;
+uint8_t mode = 0;
+
+uint8_t modifiers[] = {
+  KEY_LEFT_CTRL,
+  KEY_LEFT_SHIFT,
+  KEY_LEFT_ALT,
+  KEY_LEFT_GUI,
+  KEY_RIGHT_CTRL,
+  KEY_RIGHT_SHIFT,
+  KEY_RIGHT_ALT,
+  KEY_RIGHT_GUI
+};
+
+void setup() {
+  randomSeed(analogRead(0));  
+  pinMode(LED_BUILTIN, OUTPUT);
+  digitalWrite(LED_BUILTIN, LOW);
+  Keyboard.begin(); "›‡€ıÎÏ
+}
+
+void loop() {
+  if (mode == 0) {  
+    /* press modifiers */
+    for (uint8_t i = 0; i != 8; i++) {
+      if (modifier & (1 << i)) {
+        Keyboard.press(modifiers[i]);
+        delay(2);
+      }
+    }
+  
+    /* skip modifiers I guess */
+    if (code == 128) {
+      code = 136;
+    }
+    
+    /* press and release key */
+    Keyboard.press(code);  
+    delay(10 + random(0, 10));
+    Keyboard.release(code);
+  
+    /* release modifiers */
+    for (uint8_t i = 0; i != 8; i++) {
+      if (modifier & (1 << i)) {
+        Keyboard.release(modifiers[i]);
+        delay(2);
+      }
+    }
+  } else {
+    Keyboard.press(random(0,255));
+    delay(2);
+    Keyboard.press(random(0,255));
+    delay(2);
+    Keyboard.press(random(0,255));
+    delay(10 + random(0,10));
+    Keyboard.releaseAll();
+  }    
+  loops++;
+  modifier++;
+  code++;
+
+  if (loops == 65536) {
+    Keyboard.releaseAll();
+    digitalWrite(LED_BUILTIN, HIGH);
+    delay(500);
+    digitalWrite(LED_BUILTIN, LOW);
+    loops = 0;
+    if (mode == 0) {
+      mode = 1;
+    } else {
+      mode = 0;
+    }
+  }
+}
